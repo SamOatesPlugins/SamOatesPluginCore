89637d62d9a363d350de433adfe4da78822cfc46
samoatesgames <sam@samoatesgames.com>
2015-01-17 23:44:46 +0000

    Added a stand alone version of SamOatesPlugin.

---
+++ SamOatesPluginCore/src/main/java/com/samoatesgames/samoatesplugincore/plugin/SamOatesPluginStandalone.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samoatesgames.samoatesplugincore.plugin;
+
+/**
+ *
+ * @author Sam
+ */
+public class SamOatesPluginStandalone extends SamOatesPlugin {
+
+    public SamOatesPluginStandalone() {
+        super("SamOatesPluginStandalone");
+    }
+
+    @Override
+    public void setupConfigurationSettings() {}
+    
+}
